33b0192918bbdf60634f2b8f292b8eeabac92695
SantinoCremona <129080823+SantinoCremona@users.noreply.github.com>
2026-03-29 18:35:45 -0300

    dm

---
+++ app/stats/page.tsx
@@ -0,0 +1,10 @@
+import DreamTeam from './components/dreameam';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100">
+      {/* Tu Navbar u otros componentes */}
+      <DreamTeam />
+    </main>
+  );
+}
