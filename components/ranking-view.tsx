fa1a09f61ba0752a0c66765f8fd6d6540beae82b
SantinoCremona <129080823+SantinoCremona@users.noreply.github.com>
2026-07-01 11:01:48 -0300

    Change default selectedFecha value from 12 to 13

--- components/ranking-view.tsx
+++ components/ranking-view.tsx
@@ -22,7 +22,7 @@ export function RankingView({
 }) {
   const supabase = createClient()
   const [view, setView] = useState<"GENERAL" | "FECHA" | "CLUB">("GENERAL")
-  const [selectedFecha, setSelectedFecha] = useState(12) // Cambiado a 5 por defecto
+  const [selectedFecha, setSelectedFecha] = useState(13) // Cambiado a 5 por defecto
   const [ranking, setRanking] = useState(initialRanking)
   const [visibleCount, setVisibleCount] = useState(10)
   const [loading, setLoading] = useState(false)
